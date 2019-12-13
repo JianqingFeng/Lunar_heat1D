7d53611f0bc0335a1a1940818efb2b32c2224544
JianqingFeng <58750516+JianqingFeng@users.noreply.github.com>
2019-12-13 15:53:56 -0600

    Add files via upload

---
+++ Thermal_Model_Example.m
@@ -0,0 +1,33 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% [T,p]=heat1d_mex(H,rhos,rhod,ks,kd,lattitude,albedo,outtime)
+% T: The output temperature matrix;
+% p: The output properties of each layer, including depth, thickness and density
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+H=0.07;                            %h-parameter
+rhos=1100;                         %Surface density
+rhod=1800;                         %Density at depth
+ks=8.0e-4;                         %ks is surface conductivity
+kd=3.8e-3;                         %kd is deep conductivity
+latitude=0;                        %Latitude
+CHI=2.7;                           %Regolith radiative conductivity parameter (zero for rocks)
+Malbedo=0.07;                      %Typical mare albedo
+Halbedo=0.12;                      %Typicalhighland albedo
+outtime=0:0.02:24;                 %Times to output
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+[T,p]=heat1d_mex(H, rhos, rhod, ks, kd, CHI, latitude, Halbedo, outtime); 
+
+figure;
+plot(max(T'),p(:,1),'r',min(T'),p(:,1),'r',mean(T'),p(:,1),'r');
+ xlabel('Temperature (K)','interpreter','latex');
+ ylabel('Depth (m)','interpreter','latex');
+ set(gca,'YDir','reverse');
+
+figure;
+plot(outtime,T(1,:),'k','linewidth',1.5);
+ axis([0, 24, 50, 400]);
+ xlabel('Local Time','interpreter','latex');
+ ylabel('Temperature (K)','interpreter','latex');
+ set(gca,'xtick',0:6:24,'xticklabel',{'12:00','18:00','00:00','06:00','12:00'});
